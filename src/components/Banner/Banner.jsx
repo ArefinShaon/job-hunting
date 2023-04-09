641b6c504ac31dd6327df653fa572cda2bfe7478
ArefinShaon <arefinshaon99@gmail.com>
2023-04-10 01:20:47 +0600

    tasmim arefin

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import image from "../../assets/All Images/P3OLGJ1 copy 1.png";
+
+const Banner = () => {
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={image} className="max-w-sm rounded-lg" />
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
