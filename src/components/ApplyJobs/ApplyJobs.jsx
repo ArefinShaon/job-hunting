c7f2dffde7d92426267ae99d9a58b8338bc5acc9
ArefinShaon <arefinshaon99@gmail.com>
2023-04-11 01:52:51 +0600

    added to local storage

--- src/components/ApplyJobs/ApplyJobs.jsx
+++ src/components/ApplyJobs/ApplyJobs.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-const ApplyJobs = () => {
-    return (
-        <div>
-            <h2>Jobs</h2>
-        </div>
-    );
+const AppliedJobs = () => {
+  const [appliedJobs, setAppliedJobs] = useState([]);
+
+  useEffect(() => {
+    const storedJobs = localStorage.getItem("job");
+    if (storedJobs) {
+      setAppliedJobs(JSON.parse(storedJobs));
+    }
+  }, []);
+
+  return (
+    <div>
+      <h2>Applied Jobs</h2>
+      {appliedJobs.length > 0 ? (
+        <ul>
+          {appliedJobs.map((job) => (
+            <li key={job.id}>
+              <h3>{job.job_title}</h3>
+              <p>{job.company_name}</p>
+              <p>{job.location}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No jobs applied yet!</p>
+      )}
+    </div>
+  );
 };
 
-export default ApplyJobs;
+export default AppliedJobs;
